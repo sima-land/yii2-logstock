460d5943ebc1d91666b0cea9ef45d8e319d7bfbd
Maxim Menshenin <menshenin_m@sima-land.ru>
2017-01-31 16:12:50 +0500

    advanced filter test

--- tests/_support/ExampleFilter.php
+++ tests/_support/ExampleFilter.php
@@ -9,8 +9,8 @@ class ExampleFilter implements LogFilterInterface
     public function filter($log)
     {
         return preg_replace(
-            "/(session_id|expired_at|updated_at) [=<>] '[^']+'/",
-            '$1 = :DYNAMIC',
+            "/(session_id|expired_at|updated_at) ([=<>]) '[^']+'/",
+            '$1 $2 :DYNAMIC',
             $log
         );
     }
